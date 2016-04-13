3cf14c663fd406e55ea8f4ac71f9f7cfffe3e0a5
Jorge Mario Peláez Velásquez <jmpelaez@gmail.com>
2016-04-13 11:43:37 -0500

    Bug: moving.js - fixed missing reference to fixed header when moving

--- javascripts/moving.js
+++ javascripts/moving.js
@@ -109,9 +109,11 @@
 		//log("Moving x = " + x + ", y = " + y);
 		moving.style.left=(x < 0)? '0px' : x+'px';
 		moving.style.top=(y < 0)? '0px' : y+'px';
-    if (moving.firstChild.classList.contains("top-fixed")) {
-      var evt = new CustomEvent("scroll",{detail: {}, bubbles: true, cancelable: true});
-      moving.dispatchEvent(evt);
+    if (moving.firstChild.classList) {
+      if (moving.firstChild.classList.contains("top-fixed")) {
+        var evt = new CustomEvent("scroll",{detail: {}, bubbles: true, cancelable: true});
+        moving.dispatchEvent(evt);
+      }
     }
 	}
 
