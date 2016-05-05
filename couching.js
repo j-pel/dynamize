ff795641a51bca09711a01f33256b516f32f89e7
Jorge Mario Peláez Velásquez <jmpelaez@gmail.com>
2016-05-05 00:13:57 -0500

    Some work with couching using promises

--- couching.js
+++ couching.js
@@ -27,52 +27,56 @@ function Couching(database) {
    * @api public
    */
   self.init = function(database) {
-    var idx = database.lastIndexOf('/')+1;
-    self.db = database.slice(idx);
-    self.server = database.slice(0,idx);
-    //server: database.match(/^https?\:\/\/[^\/?#]+(?:[\/?#]|$)/i)[0];
-    var req = new XMLHttpRequest();
-    req.open('GET', self.server + self.db);
-    req.onreadystatechange=function() {
-      if (req.readyState==4) {
-        switch (req.status) {
-				case 0:
-					self.status = "no server";
-					//console.log("no server", req.responseText);
-					break;
-				case 200:
-					self.status = "database ok";
-					//console.log("open", JSON.parse(this.responseText));
-					break;
-				case 404:
-					self.status = "no database";
-					createDatabase();
-					break;
-				default:
-					self.status = "error";
-					console.log("error", req.responseText);
-				}
+    var url = database.match(/(^https?\:\/\/)([^@]+@|)([^\/?#]+(?:[\/?#]|$))([^\/?#]+(?:[\/?#]|$))/i);
+    self.protocol = url[1];
+    self.basicauth = Base64.encode(url[2].slice(0,-1));
+    self.host = url[3];
+    self.db = url[4];
+    self.promise = new Promise(function (resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.onreadystatechange=function() {
+        if (req.readyState==4) {
+          switch (req.status) {
+          case 0:
+            self.status = "no server";
+            //console.log("no server", req.responseText);
+            break;
+          case 200:
+            self.status = "database ok";
+            resolve(this.response);
+            //console.log("open", JSON.parse(this.responseText));
+            break;
+          case 404:
+            self.status = "no database";
+            createDatabase();
+            reject(this.statusText);
+            break;
+          default:
+            self.status = "error";
+            console.log("error", req.responseText);
+          }
+        }
       }
-    }
-    req.send(null);
-    return(0);
+      req.open('GET', self.protocol + self.host + self.db);
+      req.send(null);
+    });
+    return(self);
   }
 
 	self.login = function(user, callback) {
     var req = new XMLHttpRequest();
-    req.open('POST', self.server + "_session", true);
+    req.open('POST', self.protocol + self.host + "_session", true);
     req.withCredentials = true;
 		//req.setRequestHeader('Access-Control-Allow-Origin', '*');
 		//req.setRequestHeader('Accept', 'application/json');
     req.onreadystatechange=function() {
-      console.log("Headers",req.getAllResponseHeaders());
+      //console.log("Headers",req.getAllResponseHeaders());
       //console.log("Headers",req.getResponseHeader('Set-Cookie'));
       if (req.readyState==4) {
 				//console.log("Response",this);
 				obj = JSON.parse(this.responseText);
 				if (req.status == 200) {
-					self.name = user.name;
-					self.password = user.password;
+					self.basicauth = Base64.encode(user.name+":"+user.password);
 					self.roles = obj.roles
 				}
 				callback(obj);
@@ -90,7 +94,7 @@ function Couching(database) {
 
 	self.session = function(callback) {
     var req = new XMLHttpRequest();
-    req.open('GET', self.server + "_session");
+    req.open('GET', self.protocol + self.host + "_session");
     //req.setRequestHeader('X-CouchDB-WWW-Authenticate', 'Cookie');
     req.onreadystatechange=function() {
       if (req.readyState==4) {
@@ -105,6 +109,10 @@ function Couching(database) {
     return(0);
 	}
 
+	self.clear = function() {
+		console.log("deleting database ",self);
+		deleteDatabase();
+	}
   /*!
    * head(id, callback)
    * The lightest and fastest call to seek for a document knowing its
@@ -119,7 +127,7 @@ function Couching(database) {
    */
   self.head = function(id, callback) {
     var req = new XMLHttpRequest();
-    req.open('HEAD', self.server + self.db + "/" + id);
+    req.open('HEAD', self.protocol + self.host + self.db + "/" + id);
     req.onreadystatechange=function() {
       if (req.readyState==4) {
         var head = req.getAllResponseHeaders();
@@ -153,7 +161,7 @@ function Couching(database) {
       var obj = JSON.parse(this.responseText);
       callback(obj);
     }
-    req.open('GET', self.server + self.db + "/" + id);
+    req.open('GET', self.protocol + self.host + self.db + "/" + id);
     req.send();
   }
 
@@ -198,9 +206,9 @@ function Couching(database) {
       storeDoc(this, doc, callback);
     })
     if (doc._id) {
-      req.open('GET', self.server + self.db + "/" + doc._id);
+      req.open('GET', self.protocol + self.host + self.db + "/" + doc._id);
     } else {
-      req.open('GET', self.server + "/_uuids");
+      req.open('GET', self.protocol + self.host + "_uuids");
     }
     req.send();
   }
@@ -230,39 +238,23 @@ function Couching(database) {
     req.addEventListener("load",function(evt) {
       storeDoc(this, doc, callback);
     })
-    req.open('GET', self.server + "_uuids");
+    req.open('GET', self.protocol + self.host + "_uuids");
     req.send();
   }
 
   /*!
-   * view(design, view, options, callback)
+   * query(view, options, callback)
    * To query a map design/view on the database considering the
    * options' values. The resulted documents will be passed
    * to the callback function.
    * 
-   * @param {design} name of the design document.
-   * @param {view} name of the view as defined on design document.
+   * @param {view} name of the design/view as defined on database.
    * @param {options} query options as a javascript object.
    * @param {callback} function to receive CouchDB retrieved documents.
    * @api public
    */
-  self.view = function(design, view, options, callback) {
-    var req = new XMLHttpRequest();
-    req.addEventListener("load",function(evt) {
-      var obj = JSON.parse(this.responseText);
-      callback(obj);
-    });
-    var query = "";
-    for (opt in options) {
-      query += encodeURI(opt+"="+options[opt]+"&");
-    }
-    req.open('GET', self.server + self.db + "/_design/" + design 
-      + "/_view/" + view + "?" + query.slice(0,-1));
-    req.send();
-  }
-
   self.query = function(view, options, callback) {
-    if (self.status!="database ok") return(-1);
+    //if (self.status!="database ok") return(-1);
     var req = new XMLHttpRequest();
     req.onreadystatechange=function() {
       if (req.readyState==4) {
@@ -275,7 +267,7 @@ function Couching(database) {
     for (opt in options) {
       query += encodeURI(opt+"="+options[opt]+"&");
     }
-    req.open('GET', self.server + self.db + "/_design/" + view[0] 
+    req.open('GET', self.protocol + self.host + self.db + "_design/" + view[0] 
       + "/_view/" + view[1] + "?" + query.slice(0,-1));
     req.send();
   }
@@ -310,7 +302,7 @@ function Couching(database) {
       var obj = JSON.parse(this.responseText);
       callback(obj);
     });
-    req.open('PUT', self.server + self.db + "/" + obj._id);
+    req.open('PUT', self.protocol + self.host + self.db + "/" + obj._id);
     req.setRequestHeader("Content-Type", "application/json");
     req.send(JSON.stringify(obj));
   }
@@ -333,18 +325,53 @@ function Couching(database) {
       req.addEventListener("load", function(evt) {
         callback(this);
       });
-      req.open('DELETE', self.server + self.db + "/" + id + 
+      req.open('DELETE', self.protocol + self.host + self.db + "/" + id + 
         "?rev=" + head.ETag);
       req.send();
     }
   }
 
 	function createDatabase() {
-    if (!self.user) return(-1);
+    if (!self.basicauth||self.basicauth=="") return(-1);
     var req = new XMLHttpRequest();
-    req.open('PUT', self.server + self.db);
+    req.open('PUT', self.protocol + self.host + self.db);
+    req.setRequestHeader('Authorization', 'Basic ' + self.basicauth);
     req.onreadystatechange=function() {
       if (req.readyState==4) {
+        switch (req.status) {
+				case 0:
+					self.status = "no server";
+					//console.log("no server", req.responseText);
+					break;
+				case 201:
+					self.status = "database ok";
+					//console.log("open", JSON.parse(this.responseText));
+					break;
+				case 400: //400 Bad Request – Invalid database name
+				case 401: //401 Unauthorized – CouchDB Server Administrator privileges required
+				case 412: //412 Precondition Failed – Database already exists
+					self.status = "no database";
+					//console.log("creating fails for",req.status, req.responseText);
+					break;
+				default:
+					self.status = "error";
+					console.log("error", req.responseText);
+				}
+        //callback(obj);
+      }
+    }
+    req.send(null);
+    return(0);
+	}		
+
+	function deleteDatabase() {
+    if (!self.basicauth||self.basicauth=="") return(-1);
+    var req = new XMLHttpRequest();
+    req.open('DELETE', self.protocol + self.host + self.db);
+    req.setRequestHeader('Authorization', 'Basic ' + self.basicauth);
+    req.onreadystatechange=function() {
+      if (req.readyState==4) {
+					console.log("delete", req.responseText);
         switch (req.status) {
 				case 0:
 					self.status = "no server";
@@ -373,4 +400,137 @@ function Couching(database) {
 	
   self.init(database);
   return(self);
+}
+
+var Base64 = {
+	// private property
+	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+	// public method for encoding
+	encode : function (input) {
+			var output = "";
+			var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+			var i = 0;
+
+			input = Base64._utf8_encode(input);
+
+			while (i < input.length) {
+
+					chr1 = input.charCodeAt(i++);
+					chr2 = input.charCodeAt(i++);
+					chr3 = input.charCodeAt(i++);
+
+					enc1 = chr1 >> 2;
+					enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+					enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+					enc4 = chr3 & 63;
+
+					if (isNaN(chr2)) {
+							enc3 = enc4 = 64;
+					} else if (isNaN(chr3)) {
+							enc4 = 64;
+					};
+
+					output = output +
+					Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) +
+					Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
+
+			}
+
+			return output;
+	},
+
+	// public method for decoding
+	decode : function (input) {
+			var output = "";
+			var chr1, chr2, chr3;
+			var enc1, enc2, enc3, enc4;
+			var i = 0;
+
+			input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+			while (i < input.length) {
+
+					enc1 = Base64._keyStr.indexOf(input.charAt(i++));
+					enc2 = Base64._keyStr.indexOf(input.charAt(i++));
+					enc3 = Base64._keyStr.indexOf(input.charAt(i++));
+					enc4 = Base64._keyStr.indexOf(input.charAt(i++));
+
+					chr1 = (enc1 << 2) | (enc2 >> 4);
+					chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+					chr3 = ((enc3 & 3) << 6) | enc4;
+
+					output = output + String.fromCharCode(chr1);
+
+					if (enc3 != 64) {
+							output = output + String.fromCharCode(chr2);
+					}
+					if (enc4 != 64) {
+							output = output + String.fromCharCode(chr3);
+					}
+
+			}
+
+			output = Base64._utf8_decode(output);
+
+			return output;
+
+	},
+
+	// private method for UTF-8 encoding
+	_utf8_encode : function (string) {
+			string = string.replace(/\r\n/g,"\n");
+			var utftext = "";
+
+			for (var n = 0; n < string.length; n++) {
+
+					var c = string.charCodeAt(n);
+
+					if (c < 128) {
+							utftext += String.fromCharCode(c);
+					}
+					else if((c > 127) && (c < 2048)) {
+							utftext += String.fromCharCode((c >> 6) | 192);
+							utftext += String.fromCharCode((c & 63) | 128);
+					}
+					else {
+							utftext += String.fromCharCode((c >> 12) | 224);
+							utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+							utftext += String.fromCharCode((c & 63) | 128);
+					}
+
+			}
+
+			return utftext;
+	},
+
+	// private method for UTF-8 decoding
+	_utf8_decode : function (utftext) {
+			var string = "";
+			var i = 0;
+			var c = c1 = c2 = 0;
+
+			while ( i < utftext.length ) {
+
+					c = utftext.charCodeAt(i);
+
+					if (c < 128) {
+							string += String.fromCharCode(c);
+							i++;
+					}
+					else if((c > 191) && (c < 224)) {
+							c2 = utftext.charCodeAt(i+1);
+							string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+							i += 2;
+					}
+					else {
+							c2 = utftext.charCodeAt(i+1);
+							c3 = utftext.charCodeAt(i+2);
+							string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+							i += 3;
+					}
+
+			}
+			return string;
+	}
 }
