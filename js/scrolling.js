5af6f9a9d84e5e45cf7e3d9b736b326f2c514cec
Jorge Mario Peláez Velásquez <jmpelaez@gmail.com>
2021-06-21 19:49:16 -0500

    Fix: size on scrolling still to verify

--- js/scrolling.js
+++ js/scrolling.js
@@ -24,7 +24,7 @@
 				const vscroller = elements[e].parentNode;
 				const header = vscroller.previousSibling;
 				const page = vscroller.parentNode.parentNode.parentNode.parentNode;
-				vscroller.style.height = (page.offsetHeight - header.offsetHeight - header.offsetTop)+"px";
+				vscroller.style.height = (page.offsetHeight - header.offsetHeight - header.offsetTop - 10)+"px";
 			} catch(err){
 
 			}
